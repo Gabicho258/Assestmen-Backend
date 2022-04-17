7aa2b27098bf37a3398d494f323336fa8ac45d0e
Gabriel <gmamanican@unsa.edu.pe>
2022-04-17 10:09:50 -0500

    using routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import cors from "cors";
 import "dotenv/config";
 
+import { listRouter, userRouter } from "./api/routes/index.js";
+
 //db connection
 
 const dbConnection = process.env.DB_CONNECTION;
@@ -25,4 +27,12 @@ app.use(express.json());
 // Routes
 app.get("/", (request, response) => {
   response.send("FAVPS API");
+});
+app.use("/auth", userRouter);
+app.use("/api", listRouter);
+const PORT = process.env.PORT || 5000;
+
+// Launch server
+app.listen(PORT, () => {
+  console.log("Server running on port: ", PORT);
 });
